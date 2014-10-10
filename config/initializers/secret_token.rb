33de4c36c011979add8f5b76cb932da52c37c9bd
JIucToyxuu <antonduzhnov.90@gmail.com>
2014-10-10 14:43:34 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebUP::Application.config.secret_key_base = '94aec548e5c36897da966088bbc1b9dcb42fc358727dabb232adc4ae97f065cbdd65af5fc7248c494025035455c5c7b475e2c64b3dd8082beb8a75b397748227'
